0eb7c2487e0c360682fa49d5b9333d0a13515e77
RakhimyanTim <rahimtoy@gmail.com>
2020-02-23 10:00:31 +0300

    Dropdown button

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -6,6 +6,34 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  List _fruits = ["Яблоко", "Банан", "Ананас", "Манго", "Виноград"];
+
+  List<DropdownMenuItem<String>> _dropDownMenuItems;
+  String _selectedFruit;
+
+  @override
+  void initState() {
+    _dropDownMenuItems = buildAndGetDropDownMenuItems(_fruits);
+    _selectedFruit = _dropDownMenuItems[0].value;
+    super.initState();
+  }
+
+  List<DropdownMenuItem<String>> buildAndGetDropDownMenuItems(List fruits) {
+    List<DropdownMenuItem<String>> items = List();
+    for (String fruit in fruits) {
+      items.add(DropdownMenuItem(value: fruit, child: Text(fruit)));
+    }
+    return items;
+  }
+
+  void changedDropDownItem(String selectedFruit) {
+    setState(() {
+      _selectedFruit = selectedFruit;
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,7 +53,22 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Colors.white,
       ),
       backgroundColor: Colors.white,
-      body: null,
+      body: Container(
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text("Пожалуйста, выберите фрукт: "),
+              DropdownButton(
+                value: _selectedFruit,
+                items: _dropDownMenuItems,
+                onChanged: changedDropDownItem,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
